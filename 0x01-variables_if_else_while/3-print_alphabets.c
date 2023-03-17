3d3105e053262260fac26d457f92bd04daaf1ca9
amkhaoumeryem <meryemamkhaou99@gmail.com>
2023-03-17 16:33:50 -0700

    variables_if_eles_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-        int ch;
-        for (ch = 'a'; ch <= 'z'; ch++)
-                putchar(ch);
-        for (ch = 'A'; ch <= 'Z'; ch++)
-                putchar(ch);
-        putchar('\n');
-        return (0);
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
